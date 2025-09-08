b918f94479c5fa9d2bf0f6d9aeefa5f90e6ba591
Wojs, Michal <wojs.michal@gmail.com>
2025-09-08 11:39:54 +0200

    Add new assets, reorganize view

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,26 @@ navToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
   this.classList.toggle("active");
 });
+
+let slideIndex = 0;
+let slideTimer;
+
+function showSlides() {
+  let slides = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+
+  slideTimer = setTimeout(showSlides, 5000); // 5s między zdjęciami
+}
+
+function plusSlides(n) {
+  clearTimeout(slideTimer); // zatrzymanie automatu
+  slideIndex += n - 1;      // poprawne przesunięcie
+  showSlides();
+}
+
+showSlides();
